57d50cfcfa3e869af8e0d5282c3634a9c4dacec0
Artas <gillartas@googlemail.com>
2021-07-12 23:26:41 +0000

    service 2 & 3 completed

---
+++ make_api/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, Response
+import random 
+
+app = Flask(__name__)
+
+@app.route('/get_make', methods=['GET'])
+def get_make():
+    make = random.choice(['BMW','Mercedes','Vauxhall','Honda','Audi','Volkswagen'])
+    return Response(make)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
---
+++ make_api/tests/test_make_api.py
@@ -0,0 +1,15 @@
+from flask import url_for
+from flask_testing import TestCase
+from app import app
+from unittest.mock import patch
+
+class TestBase(TestCase):
+    def create_app(self):
+        return app
+
+class TestHome(TestBase):
+    def test_get_make(self):
+        for i in range(6):
+            response = self.client.get(url_for('get_make'))
+            make_list = [b'BMW',b'Mercedes',b'Vauxhall',b'Honda',b'Audi',b'Volkswagen']
+            self.assertIn(response.data, make_list)
