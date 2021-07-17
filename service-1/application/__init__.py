999dfb1cfc99b6095991f301d25f0948c439bc87
Artas <gillartas@googlemail.com>
2021-07-17 21:28:41 +0000

    testing all completed

--- service-1/application/__init__.py
+++ service-1/application/__init__.py
@@ -4,6 +4,7 @@ from os import getenv
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']= getenv('DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db = SQLAlchemy(app)
 
 from application import routes  
