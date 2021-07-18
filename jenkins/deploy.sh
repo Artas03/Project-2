95bae573128130b660911014deaa222f47c690d1
Artas <gillartas@googlemail.com>
2021-07-18 17:34:11 +0000

    jenkins

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # copy docker-compose.yaml onto manager node
-scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@instance-1:/home/jenkins/docker-compose.yaml
+scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@instance-1:/home/workspace/docker-compose.yaml
 
 # docker stack deploy
 ssh -i ~/.ssh/ansible_id_rsa jenkins@instance-1 << EOF
