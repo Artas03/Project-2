7ee5a0e702a842a142ae835c2978949c51c1471e
Artas <gillartas@googlemail.com>
2021-07-18 17:10:31 +0000

    jenkins

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # copy docker-compose.yaml onto manager node
-scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@manager:/home/jenkins/docker-compose.yaml
+scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@instance-1:/home/jenkins/docker-compose.yaml
 
 # docker stack deploy
-ssh -i ~/.ssh/ansible_id_rsa jenkins@manager << EOF
+ssh -i ~/.ssh/ansible_id_rsa jenkins@instance-1 << EOF
     export DATABASE_URI=${DATABASE_URI}
     docker stack deploy --compose-file docker-compose.yaml project2
 EOF 
