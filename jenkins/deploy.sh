317ba7d6524eee832bfff6d4a654eb08c89e7ada
Artas03 <84901911+Artas03@users.noreply.github.com>
2021-07-18 10:55:04 +0100

    Delete jenkins directory

--- jenkins/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@manager:/home/jenkins/docker-compose.yaml
-
-ssh -i ~/.ssh/ansible_id_rsa jenkins@manager << EOF
-    export DATABASE_URI=${DATABASE_URI}
-    docker stack deploy --compose-file docker-compose.yaml project2
-
-EOF
